7565dd390fb1a853efab7d9b1b42286b0629c2fc
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-12-10 19:20:42 -0500

    add compartments

--- puzzle-03-tests/TestRusksack.cs
+++ puzzle-03-tests/TestRusksack.cs
@@ -6,10 +6,21 @@ namespace puzzle_03_tests;
 public class TestRusksack
 {
     [Fact]
-    public void CreateRucksack()
+    public void CreateRucksack_Data()
     {
-        var data = "vJrwpWtwJgWrhcsFMMfFFhFp";
+        var data = "a23b47cd";
         var rucksack = new Rucksack(data);
         rucksack.Data.Should().Be(data);
+    }
+    
+    [Fact]
+    public void CreateRucksack_Compatements()
+    {
+        var leftData = "a23b";
+        var rightData = "47cd";
+        var data = leftData + rightData;
+        var rucksack = new Rucksack(data);
+        rucksack.FirstCompartment.Should().Be(leftData);
+        rucksack.SecondCompartment.Should().Be(rightData);
     }
 }
--- puzzle-03/Rucksack.cs
+++ puzzle-03/Rucksack.cs
@@ -1,11 +1,17 @@
+using Microsoft.VisualBasic;
+
 namespace puzzle_03;
 
 public class Rucksack
 {
     public string Data { get; private set; }
+    public string FirstCompartment { get; private set; }
+    public string SecondCompartment { get; private set; }
 
     public Rucksack(string data)
     {
         Data = data;
+        FirstCompartment = data.Substring(0, data.Length / 2);
+        SecondCompartment = data.Substring(data.Length / 2, data.Length / 2);
     }
 }
